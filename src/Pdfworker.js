898adf77ed9dfa8be17b8e78b952a157cda9a8f0
murat colak <colakmurat-78@hotmail.com>
2026-03-02 21:19:37 +0100

    pdf_gelöst

--- src/Pdfworker.js
+++ src/Pdfworker.js
@@ -1,7 +1,4 @@
-// src/pdfWorker.js
 import { pdfjs } from "react-pdf";
 
-// ✅ Worker'ı pdfjs-dist içinden al (react-pdf ile aynı dependency ağacından)
-import workerSrc from "pdfjs-dist/build/pdf.worker.min.mjs?url";
-
-pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
+pdfjs.GlobalWorkerOptions.workerSrc =
+    `https://unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
