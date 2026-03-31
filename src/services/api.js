5e79b538da3aa44ea6c6d16812aeb1a810106743
murat colak <colakmurat-78@hotmail.com>
2026-03-31 09:04:38 +0200

    env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // services/api.js
 import axios from 'axios';
 
-const API_URL = 'https://angebot-backend-ub6a.onrender.com/api';
+const API_URL = import.meta.env.VITE_API_URL + '/api';
 
 const api = axios.create({
     baseURL: API_URL,
